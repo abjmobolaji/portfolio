ec9a6a0e6124d10e149a6e5447e4f62100df2e4d
leboshjr <abjmobolaji@gmail.com>
2020-08-26 03:24:07 +0100

    Added Testimonials

---
+++ script.js
@@ -0,0 +1,48 @@
+/** Declaring Variables **/
+const toggle = document.querySelector(".toggle");
+const navbar = document.querySelector(".navbar");
+ 
+/** Toggle mobile menu **/
+function toggleMenu() {
+    if (navbar.classList.contains("active")) {
+        navbar.classList.remove("active");
+         
+        // adds the menu (hamburger) icon
+        toggle.querySelector("a").innerHTML = "<i class=’fas fa-bars’></i>";
+    } else {
+        navbar.classList.add("active");
+         
+        // adds the close (x) icon
+        toggle.querySelector("a").innerHTML = "<i class=’fas fa-times’></i>";
+    }
+}
+ 
+/** Event Listener **/
+toggle.addEventListener("click", toggleMenu, false);
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" activedot", "");
+    }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " activedot";
+}
